f615d7e7e6b1f3154c39ff732deabff72f594def
YQisme <1398757912@qq.com>
2023-10-24 15:50:16 +0800

    设置信息

--- next.config.js
+++ next.config.js
@@ -2,6 +2,45 @@
 const nextConfig = {
   reactStrictMode: true,
   basePath: "/knowledge",
+  async rewrites() {
+    return [
+      {
+        source: "/",
+        destination: "/index.html",
+      },
+      {
+        source: "/:path*",
+        destination: `/:path*`,
+      },
+      // 添加blog
+      {
+        source: "/blog",
+        destination: `https://blog-psi-pied-75.vercel.app/blog`,
+      },
+      {
+        source: "/blog/:path*",
+        destination: `https://blog-psi-pied-75.vercel.app/blog/:path*`,
+      },
+      // 添加doc
+      {
+        source: "/doc",
+        destination: `https://doc-ten-lilac.vercel.app`,
+      },
+      {
+        source: "/doc/:path*",
+        destination: `https://doc-ten-lilac.vercel.app/:path*`,
+      },
+      // 添加knowledge
+      {
+        source: "/knowledge",
+        destination: `https://knowledge-eosin-theta.vercel.app/`,
+      },
+      {
+        source: "/knowledge/:path*",
+        destination: `https://knowledge-eosin-theta.vercel.app/:path*`,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
